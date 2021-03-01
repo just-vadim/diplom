f5552a0e0830313af67630f742c30b78fdabffe5
Vadim <vadim.kurn@gmail.com>
2021-03-01 22:14:16 +0300

    StartPage.java developed

--- src/test/java/ru/netology/page/StartPage.java
+++ src/test/java/ru/netology/page/StartPage.java
@@ -1,4 +1,30 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class StartPage {
+    private SelenideElement heading = $(".heading_size_l");
+    private SelenideElement paymentByCardButton = $$("[type='button']").find(exactText("Купить"));
+    private SelenideElement paymentOnCreditButton = $$("[type='button']").find(exactText("Купить в кредит"));
+
+    public StartPage() {
+        heading.shouldBe(visible);
+        paymentByCardButton.shouldBe(visible);
+        paymentOnCreditButton.shouldBe(visible);
+    }
+
+    public PaymentByCardPage selectPaymentByCardPage() {
+        paymentByCardButton.click();
+        return new PaymentByCardPage();
+    }
+
+    public PaymentOnCreditPage selectPaymentOnCreditPage() {
+        paymentOnCreditButton.click();
+        return new PaymentOnCreditPage();
+    }
 }

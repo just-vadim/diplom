37771f959e63dd347de79f4e91b49704b10a27e3
Vadim <vadim.kurn@gmail.com>
2021-03-07 23:44:38 +0300

    SQLHelper.java edited

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -5,13 +5,11 @@ import org.apache.commons.dbutils.QueryRunner;
 import java.sql.*;
 
 public class SQLHelper {
-    private static String url = System.getProperty("db.url");
-    private static String user = System.getProperty("db.user");
-    private static String password = System.getProperty("db.password");
+    private static String url = "jdbc:mysql://localhost:3306/app";
+//    private static String url = "jdbc:postgresql://localhost:5432/app";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                url, user, password);
+        return DriverManager.getConnection(url, "app", "pass");
     }
 
     public static String getPaymentByCardStatus() {
